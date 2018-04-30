f87337cd1f42454612f57dd046a17f81670844e1
Julian <jhernandeza@gmail.com>
2018-04-30 10:43:58 -0500

    3. Bases Node: Enviar parametros por consola - Actualizamos app.js

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,8 +1,15 @@
 
 const { crearArchivo } = require('./multiplicar/multiplicar');
 
-let base = 9;
 
+
+
+//console.log(process.argv);
+let argv = process.argv;
+let parametro = argv[2];
+let base = parametro.split('=')[1]
+
+console.log(base);
 crearArchivo(base)
-    .then(archivo => console.log(`Archivo creado: ${ archivo }`))
-    .catch(err => console.log(err));
+     .then(archivo => console.log(`Archivo creado: ${ archivo }`))
+     .catch(err => console.log(err));
