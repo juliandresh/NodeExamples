17fd1ea94427e70a474d05076c396684fd0e5336
Julian <jhernandeza@gmail.com>
2018-04-30 12:57:14 -0500

    3. Bases Node: Uso y optimizaciones para la configuración de Yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,15 +1,22 @@
 
-const { crearArchivo } = require('./multiplicar/multiplicar');
+const argv = require('./config/yargs').argv;
 
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
 
+let comando = argv._[0];
 
+switch (comando) {
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+    break;
 
-//console.log(process.argv);
-let argv = process.argv;
-let parametro = argv[2];
-let base = parametro.split('=')[1]
+    case 'crear':
+        crearArchivo(argv.limite, argv.base)
+        .then(archivo => console.log(`Archivo creado: ${ archivo }`))
+        .catch(err => console.log(err));    
+    break;
 
-console.log(base);
-crearArchivo(base)
-     .then(archivo => console.log(`Archivo creado: ${ archivo }`))
-     .catch(err => console.log(err));
+    default:
+    console.log('Comando no reconocido');        
+}
+
--- 03-bases-node/multiplicar/multiplicar.js
+++ 03-bases-node/multiplicar/multiplicar.js
@@ -3,7 +3,13 @@
 const fs = require('fs');
 
 
-let crearArchivo = (base) => {
+let listarTabla = ( base, limite = 10) => {
+for (let i = 1; i <= limite; i++) {
+        console.log(`${base} * ${i} = ${base*i}`);        
+    }
+}
+
+let crearArchivo = (base, limite = 10) => {
 
     return new Promise((resolve, reject) => {
         
@@ -14,21 +20,22 @@ let crearArchivo = (base) => {
 
         let data = '';
         
-        for (let i = 1; i <= 10; i++) {
+        for (let i = 1; i <= limite; i++) {
             data += `${base} * ${i} = ${base*i}\n`;    
         }
         
-        fs.writeFile(`tablas/tabla-${base}.txt`,data, (err) => {
+        fs.writeFile(`tablas/tabla-${ base }-al-${ limite }.txt`,data, (err) => {
             
             if(err)
                 reject (err)
             else
-                resolve(`tabla-${base}.txt`);
+                resolve(`tabla-${ base }-al-${ limite }.txt`);
         
         });
     });
 }
 
 module.exports = {
-    crearArchivo
+    crearArchivo,
+    listarTabla
 }
